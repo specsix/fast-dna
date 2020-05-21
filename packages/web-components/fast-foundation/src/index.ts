f30d9989bad306259994875c239c7d3f2dd2fe42
Stephane Comeau <scomea@microsoft.com>
2020-05-21 13:43:51 -0700

    stub in files

--- packages/web-components/fast-foundation/src/index.ts
+++ packages/web-components/fast-foundation/src/index.ts
@@ -21,6 +21,7 @@ export * from "./switch/index";
 export * from "./tabs/index";
 export * from "./text-area/index";
 export * from "./text-field/index";
+export * from "./tooltip/index";
 
 // export our utilities
 export * from "./utilities/index";
